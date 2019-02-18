c4f5917ac7dcf57c347defb9c64e0a91dead06e1
Dylan <woodworth.dylan@gmail.com>
2019-02-18 16:18:50 -0500

    Added the ability to favorite cards. Card is updated in database.

--- app/src/main/java/com/example/dylan/affirmationcardsapp/Card.java
+++ app/src/main/java/com/example/dylan/affirmationcardsapp/Card.java
@@ -1,5 +1,6 @@
 package com.example.dylan.affirmationcardsapp;
 
+import io.objectbox.Box;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
 
@@ -7,6 +8,7 @@ import io.objectbox.annotation.Id;
 public class Card {
     private int image;
     private boolean free = true;
+    private boolean favorite;
 
     @Id
     private long id;
@@ -16,6 +18,15 @@ public class Card {
         this.free = isFree;
     }
     public Card() { }
+
+
+    //If the user clicks the favorite button, it will swap the value of the boolean favorite value.
+    public void setFavorite(boolean b){
+
+        favorite = b;
+        Box<Card> cardBox = App.getApp().getBoxStore().boxFor(Card.class);
+        cardBox.put(this);
+    }
 
 
     public Card(int image){
@@ -34,5 +45,8 @@ public class Card {
     }
     public boolean isFree(){
         return this.free;
+    }
+    public boolean isFavorite(){
+        return favorite;
     }
 }
--- app/src/main/java/com/example/dylan/affirmationcardsapp/CollectionEnlarge.java
+++ app/src/main/java/com/example/dylan/affirmationcardsapp/CollectionEnlarge.java
@@ -4,7 +4,13 @@ import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.like.LikeButton;
+import com.like.OnLikeListener;
 
 import java.util.List;
 
@@ -14,12 +20,14 @@ import io.objectbox.BoxStore;
 public class CollectionEnlarge extends AppCompatActivity {
     Drawable d;
     public static final String EXTRA_CARD_ID = "id";
-
+    private Card card;
+    private LikeButton likeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection_enlarge);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        likeListener();
 
         BoxStore boxStore = App.getApp().getBoxStore();
         Box<Card> cardBox = boxStore.boxFor(Card.class);
@@ -27,14 +35,44 @@ public class CollectionEnlarge extends AppCompatActivity {
         ImageView cardView=findViewById(R.id.enlargedCard);
         int cardId = (Integer) getIntent().getExtras().get(EXTRA_CARD_ID);
 
-
+        card = cards.get(cardId);
+        if(card.isFavorite()){
+            likeButton.setLiked(true);
+        }
         cardView.setImageResource(cards.get(cardId).getImage());
 
+
     }
+
 
     public void setDrawable(Drawable d){
         this.d = d;
     }
+
+
+    public void likeListener(){
+        likeButton = findViewById(R.id.heart_button);
+        likeButton.setOnLikeListener(new OnLikeListener() {
+            @Override
+            public void liked(LikeButton likeButton) {
+                card.setFavorite(true);
+                TextView tv = findViewById(R.id.tv);
+
+                tv.setText("" + card.isFavorite());
+            }
+
+            @Override
+            public void unLiked(LikeButton likeButton) {
+                card.setFavorite(false);
+                TextView tv = findViewById(R.id.tv);
+
+                tv.setText("" + card.isFavorite());
+
+            }
+        });
+    }
+
+
 
 
 }
